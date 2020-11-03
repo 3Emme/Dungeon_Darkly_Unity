6245471904f1d9c1361ee2a59793082479d3292c
Ben Stoller <stollersystemsound@gmail.com>
2020-11-03 12:11:19 -0800

    bleh

--- DD_Unity_1/Assets/Models/GameElements/Actions/Get.cs
+++ DD_Unity_1/Assets/Models/GameElements/Actions/Get.cs
@@ -1,24 +1,24 @@
 namespace Dungeon_Darkly
 {
-    public partial class Action
-    {
-      
-    }
-}
-//   get(target) {
-//     let current_location = this.environments[this.players[0].location];
-//     for (let i=0;i<current_location.items.length;i++) {
-//       if (current_location.items[i].name.toLowerCase().includes(target)) {        
-//         //this.look("")
-//         Display.output(`[+] You pick up the ${current_location.items[i].name}`);
-//         this.players[0].inv.push(current_location.items[i]);
+  public partial class Action
+  {
+    //   public static void get(string target) {
+    //     Environment current_location = TerminalManager.game.Environments[TerminalManager.game.Players[0].Location];
+    //     for (int i=0;i<current_location.Items.Count;i++) {
+    //       if (current_location.Items[i].Name.ToLower().Contains(target)) {        
+    //         //this.look("")
+    //         Display.output(`[+] You pick up the ${current_location.items[i].name}`);
+    //          Interpreter.DisplayOutput($"[+] You pick up the {current_location.Items[i].Name});
+    //         TerminalManager.game.Players[0].Inv.Add(current_location.Items[i]);
 
-//         current_location.items.splice(i,1);
-//         console.log("location items:",current_location.items);
-//         console.log("INV",this.players[0].inv);
-//         // current_location.items = newArray        
-//         break;
-//       }   
-//     } 
-//     this.updateInvDisplay(); 
-//   } 
+    //         current_location.items.splice(i,1);
+    //         console.log("location items:",current_location.items);
+    //         console.log("INV",this.players[0].inv);
+    //         // current_location.items = newArray        
+    //         break;
+    //       }   
+    //     } 
+    //     Action.UpdateInvDisplay(); 
+    //   } 
+  }
+}
